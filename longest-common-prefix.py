f196b02c8c856b7c03eff7a3ce068a06c38baa86
Emma <emma.a.kellam@gmail.com>
2024-01-29 17:59:13 -0500

    Watched Neetcode's video explaining this problem and I wanted to add his solution for future reference as well

--- longest-common-prefix.py
+++ longest-common-prefix.py
@@ -68,6 +68,21 @@ sol = Solution()
 
 print(sol.longestCommonPrefix(["flower","flow","flight"]))
 
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# January 29th, 2024
+# Neetcode Solution
+
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+    	res = ""
+
+    	for i in range(len(strs[0])):
+    		for s in strs:
+    			if i == len(s) or s[i] != strs[0][i]:
+    				return res
+    			res += strs[0][i]
+    	return res
 
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
